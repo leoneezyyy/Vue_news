14895a61f4dd835b7401c1f2ff0c9e3df1027cae
leoneezyy <44772792+leoneezyy@users.noreply.github.com>
2019-09-26 19:41:35 +0800

    上传图片到服务器

--- src/pages/EditProfile.vue
+++ src/pages/EditProfile.vue
@@ -37,6 +37,30 @@ export default {
     CellBar
   },
 
+  methods: {
+    afterRead(file) {
+      // 构造表单数据
+      const formData = new FormData();
+      // 通过表单使用append方法追加数据
+      formData.append("file", file.file);
+
+      this.$axios({
+        url: "/upload",
+        methods: "POST",
+        // 添加头信息
+        headers: {
+          Authorization: localStorage.getItem('token')
+        },
+        data: formData
+      }).then(res => {
+        const {data} = res.data;
+
+        // 替换用户资料的头像
+        this.profile.head_img = this.$axios.defaults.baseURL + data.url;
+      });
+    }
+  },
+
   mounted() {
     // 请求个人资料接口
     this.$axios({
